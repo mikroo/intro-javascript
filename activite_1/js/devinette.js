a1a3c5ab5bac46103a7a3315e0737abc200d5ee4
Ch. Ntimba <ch.ntimba@MacBook-Pro-de-Ch.local>
2016-10-29 22:05:36 +0200

    creation du projet mini-jeu de devinette

---
+++ activite_1/js/devinette.js
@@ -0,0 +1,16 @@
+/* 
+Activité : jeu de devinette
+*/
+
+// NE PAS MODIFIER OU SUPPRIMER LES LIGNES CI-DESSOUS
+// COMPLETEZ LE PROGRAMME UNIQUEMENT APRES LE TODO
+
+console.log("Bienvenue dans ce jeu de devinette !");
+
+// Cette ligne génère aléatoirement un nombre entre 1 et 100
+var solution = Math.floor(Math.random() * 100) + 1;
+
+// Décommentez temporairement cette ligne pour mieux vérifier le programme
+//console.log("(La solution est " + solution + ")");
+
+// TODO : complétez le programme
